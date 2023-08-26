0d4a33683507853df4a0f363aade25b25d81bf6d
cocktailpeanut <cocktailpeanuts@proton.me>
2023-08-25 21:30:16 -0400

    init

---
+++ pinokio.js
@@ -0,0 +1,42 @@
+const os = require('os')
+const fs = require('fs')
+const path = require("path")
+const exists = (filepath) => {
+  return new Promise(r=>fs.access(filepath, fs.constants.F_OK, e => r(!e)))
+}
+module.exports = {
+  title: "facefusion",
+  description: "Next generation face swapper and enhancer",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installed = await exists(path.resolve(__dirname, "facefusion", "env"))
+    if (installed) {
+      let session = (await kernel.loader.load(path.resolve(__dirname, "session.json"))).resolved
+      return [{
+        when: "start.json",
+        on: "<i class='fa-solid fa-spin fa-circle-notch'></i> Running",
+        type: "label",
+        href: "start.json"
+      }, {
+        when: "start.json",
+        off: "<i class='fa-solid fa-power-off'></i> Launch",
+        href: "start.json?fullscreen=true&run=true",
+      }, {
+        when: "start.json",
+        on: (session && session.url ? "<i class='fa-solid fa-rocket'></i> Open Web UI" : null),
+        href: (session && session.url ? session.url : null),
+        target: "_blank"
+      }, {
+        when: "start.json",
+        on: "<i class='fa-solid fa-desktop'></i> Server",
+        href: "start.json?fullscreen=true"
+      }]
+    } else {
+      return [{
+        html: '<i class="fa-solid fa-plug"></i> Install',
+        type: "link",
+        href: "install.json?run=true&fullscreen=true"
+      }]
+    }
+  }
+}
